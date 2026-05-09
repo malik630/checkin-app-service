782a3c085d1b504f989c47687512fce7d6273693
Melliti Abdelmalek <ma_melliti@esi.dz>
2026-05-09 23:38:31 +0100

    Added auth middleware to booking et flight routes

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,6 @@ import { RequestHandler, Router } from 'express'
 import bookingsRoutes from './modules/bookings/bookings.routes.js'
 import flightsRoutes from './modules/flights/flights.routes.js'
 import authRoutes from './modules/auth/auth.routes.js'
-
 /*import seatsRoutes from './modules/seats/seats.routes.js'
 import checkinRoutes from './modules/checkin/checkin.routes.js'
 import boardingRoutes from './modules/boarding/boarding.routes.js'
@@ -12,10 +11,11 @@ import notificationsRoutes from './modules/notifications/notifications.routes.js
 import { authMiddleware } from './middleware/auth.middleware.js'
 
 const router = Router()
-router.use('/bookings', bookingsRoutes)
-router.use('/flights', flightsRoutes)
+
 router.use('/auth', authRoutes)
 
+router.use('/bookings', authMiddleware as RequestHandler, bookingsRoutes)
+router.use('/flights', authMiddleware as RequestHandler, flightsRoutes)
 
 /*router.use('/seats', seatsRoutes)
 router.use('/checkin', checkinRoutes)
