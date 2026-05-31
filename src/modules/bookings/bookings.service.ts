48e5df2525e804ad8ec8372f7557732bf02e21b4
Djerfi Fatma <154927228+fatmakhadidja@users.noreply.github.com>
2026-05-31 19:01:42 +0100

    added new seed and changes booking fetching to fetch only the ones where checkin is complete and display in bookingscreen

--- src/modules/bookings/bookings.service.ts
+++ src/modules/bookings/bookings.service.ts
@@ -36,6 +36,7 @@ export const getUpcomingBookings = async (uid: string) => {
       where: {
         checkinSession: {
           uid,
+          currentStep: 'COMPLETED',
         },
       },
       include: {
